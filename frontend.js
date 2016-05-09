74900f579b2faf3cee3db089c45ae702a88251e8
markpeace <mark@learnsomestuff.com>
2016-05-09 19:56:47 +0100

    trying to fix web server

--- frontend.js
+++ frontend.js
@@ -1,10 +1,3 @@
-http.get("https://github.com/markpeace/BrewBox/blob/master/pages/index.html", function(res) {
-  res.on('data', function(data) {
-    console.log(data);
-  });
-});
-
-
 var http = require("http");
 
 http.createServer(function(req,res){
