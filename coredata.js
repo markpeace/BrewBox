91d7c2e0865647c399228506a94e0b4f8967118a
markpeace <mark@learnsomestuff.com>
2016-05-09 20:24:27 +0100

    better coredata definition

--- coredata.js
+++ coredata.js
@@ -1,4 +1,9 @@
-exports.temperature = {
-  target:50,
-  current:10
+exports = {
+  temperature: {
+    target:50,
+    current:10,
+    time_at_current_target: 10,
+  },
+  pump_level: 10,
+  heating_active:false
 }
