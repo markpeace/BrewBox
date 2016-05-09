dbdf9a7f2dac1ad8b17b2faa0b80214cb3331272
markpeace <mark@learnsomestuff.com>
2016-05-09 19:59:46 +0100

    remote index

--- frontend.js
+++ frontend.js
@@ -1,13 +1,15 @@
 var http = require("http");
 
+var page = ""
+
 http.get("https://github.com/markpeace/BrewBox/blob/master/pages/index.html", function(res) {
   res.on('data', function(data) {
-    console.log(data);
+    page=page+data
   });
 });
 
 
 http.createServer(function(req,res){
   res.writeHead(200);
-  res.end("Has this worked");
+  res.end(data);
 }).listen(80);
