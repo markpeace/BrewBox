eac081b9e3d99ede31b192a21103cc370eff207b
markpeace <mark@learnsomestuff.com>
2016-05-09 20:16:48 +0100

    created coredata

---
+++ coredata.js
@@ -0,0 +1,4 @@
+exports.temperature = {
+  target:50,
+  current:10
+}
--- frontend.js
+++ frontend.js
@@ -2,7 +2,5 @@ var http = require("http");
 
 http.createServer(function(req,res){
   res.writeHead(200);
-  res.end(page);
+  res.end(coredata.temperature.target);
 }).listen(80);
-
-exports.testvar = "mark";
