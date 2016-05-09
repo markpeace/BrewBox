2ea4d6ad6920a1f66554af57314aa4d8ba64ed07
markpeace <mark@learnsomestuff.com>
2016-05-09 19:58:54 +0100

    mocking up pages

--- frontend.js
+++ frontend.js
@@ -1,5 +1,12 @@
 var http = require("http");
 
+http.get("https://github.com/markpeace/BrewBox/blob/master/pages/index.html", function(res) {
+  res.on('data', function(data) {
+    console.log(data);
+  });
+});
+
+
 http.createServer(function(req,res){
   res.writeHead(200);
   res.end("Has this worked");
