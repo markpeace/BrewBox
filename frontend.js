d315dbcd9c0fa2e4c89e3dc20f74b037f089644e
markpeace <mark@learnsomestuff.com>
2016-05-10 19:43:34 +0100

    fixed wrong variable name

--- frontend.js
+++ frontend.js
@@ -3,7 +3,7 @@ var http = require("http");
 var request_pages = ["index"]
 var page = {}
 
-pages.forEach(function(request_page) {
+request_pages.forEach(function(request_page) {
   http.get("https://rawgit.com/markpeace/BrewBox/master/pages/"+request_page+".html", function(res) {
     res.on('data', function(data) { page[request_page]+=data })
     res.on('close', function() {
