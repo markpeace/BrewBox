ff03a899ecda9d271a58e54921fe3ba006257a64
markpeace <mark@learnsomestuff.com>
2016-05-10 19:18:15 +0100

    .

--- frontend.js
+++ frontend.js
@@ -1,10 +1,12 @@
 var http = require("http");
 
+var page = "";
 var http = require("http");
 http.get("https://rawgit.com/markpeace/BrewBox/master/pages/index.html", function(res) {
   res.on('data', function(data) {
-    console.log(data);
+    page=page+data
   });
+  res.on('close', function() { console.log(page) })
 });
 
 http.createServer(function(req,res){
