dd3998f419adb008fabdb2ecc55e16d5ff434969
markpeace <mark@learnsomestuff.com>
2016-05-10 09:54:40 +0100

    Removed coredata reference

--- frontend.js
+++ frontend.js
@@ -3,6 +3,7 @@ var http = require("http");
 http.createServer(function(req,res){
   res.writeHead(200);
   res.write('temp');
-  res.write(coredata.temperature.actual);
-  res.end(coredata.heating_active);
+  //res.write(coredata.temperature.actual);
+  //res.end(coredata.heating_active);
+  res.end("finished");
 }).listen(80);
--- temperatures.js
+++ temperatures.js
@@ -1,6 +1,6 @@
 setInterval(function() {
 
-  coredata.heating_active = (coredata.temperature.actual<coredata.temperature.target ? true : false)
-  coredata.temperature.actual +=1
+  //coredata.heating_active = (coredata.temperature.actual<coredata.temperature.target ? true : false)
+  //coredata.temperature.actual +=1
 
 },1000)
