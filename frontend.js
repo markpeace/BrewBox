2a219a0601f442de66f19df0d98d88d3a1ba8a69
markpeace <mark@learnsomestuff.com>
2016-05-10 19:17:21 +0100

    trying to scrape html

--- frontend.js
+++ frontend.js
@@ -1,5 +1,12 @@
 var http = require("http");
 
+var http = require("http");
+http.get("https://rawgit.com/markpeace/BrewBox/master/pages/index.html", function(res) {
+  res.on('data', function(data) {
+    console.log(data);
+  });
+});
+
 http.createServer(function(req,res){
   res.writeHead(200);
   res.write(coredata.temperature.actual);
