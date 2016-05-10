68b197cc162ee5c0c5338054cd06dc2a0e8cc082
markpeace <mark@learnsomestuff.com>
2016-05-10 19:45:44 +0100

    serving the right file

--- frontend.js
+++ frontend.js
@@ -6,14 +6,10 @@ var page = {}
 request_pages.forEach(function(request_page) {
   http.get("https://rawgit.com/markpeace/BrewBox/master/pages/"+request_page+".html", function(res) {
     res.on('data', function(data) { page[request_page]+=data })
-    res.on('close', function() {
-      console.log(request_page)
-      console.log(page)
-    })
   })
 })
 
 http.createServer(function(req,res){
   res.writeHead(200);
-  res.end(page);
+  res.end(page.index);
 }).listen(80);
