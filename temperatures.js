b2362853957dbe7432e5c5ccde1baa86fe1e2c26
markpeace <mark@learnsomestuff.com>
2016-05-10 09:56:20 +0100

    Simplified

--- temperatures.js
+++ temperatures.js
@@ -1,6 +1,6 @@
-setInterval(function() {
+//setInterval(function() {
 
   //coredata.heating_active = (coredata.temperature.actual<coredata.temperature.target ? true : false)
   //coredata.temperature.actual +=1
 
-},1000)
+//},1000)
